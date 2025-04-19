2f4a6a00fc0fd0714eaf6aa4f383845670a454f4
Amir Neghabi <Amirrezaneghabi001@gmail.com>
2025-04-19 16:43:36 +0330

    add BookCreateView

--- books/urls.py
+++ books/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import BookListView,BookDetailView
+from .views import BookListView, BookDetailView, BookCreateView
 
 urlpatterns = [
     # ویو برای نمایش صفحه اول سایت که حاوی لیست کتاب هاست
     path('',BookListView.as_view(),name='book_list'),
 #     ویو برای نمایش صفحه جزییات هر کتاب
     path('<int:pk>',BookDetailView.as_view(),name='book_detail'),
+    path('create/',BookCreateView.as_view(),name='book_create'),
 ]
