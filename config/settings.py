a4a8a3ecb1984b7e943dbda3c75936f1c538f3f6
Amir Neghabi <Amirrezaneghabi001@gmail.com>
2025-06-02 13:29:27 +0330

    add fronted template and static file

--- config/settings.py
+++ config/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -147,5 +147,9 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 # email config :
 EMAIL_BACKEND  = "django.core.mail.backends.console.EmailBackend"
+
+# static fields config
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [str(BASE_DIR.joinpath("static"))]
 
 
