24c8ee0d3d736cfe7e850540b5fca08c8a48c416
Amir Neghabi <Amirrezaneghabi001@gmail.com>
2025-07-03 12:59:48 +0330

    add relared to book fk in Comment model

--- books/admin.py
+++ books/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from .models import Book,Comment
 
+# class CommentAdmin(admin.ModelAdmin):
+#     list_display = ['user','book','text','datetime_created']
+# admin.site.register(Book)
+# admin.site.register(Comment,CommentAdmin)
+# راه دوم نمایش نظرات در پنل ادمین
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['user','book','text','datetime_created']
+
 admin.site.register(Book)
