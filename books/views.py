7b768fbe26aa804eb93f02471be398137dd9d132
Amir Neghabi <Amirrezaneghabi001@gmail.com>
2025-07-10 00:07:55 +0330

    add Login Required to views

--- books/views.py
+++ books/views.py
@@ -6,6 +6,10 @@ from django.urls import reverse_lazy
 # معیین کردن مدلی که قراره براش ویو بسازیم
 from .models import Book
 from.forms import CommentForm
+# برای بررسی ثبت نام کاربر در کلاس ویوها
+from django.contrib.auth.mixins import LoginRequiredMixin
+# برای بررسی ثبت نام کاربر در functionalviews
+from django.contrib.auth.decorators import login_required
 
 
 
@@ -25,6 +29,7 @@ class BookListView(generic.ListView):
 # Book_list_view حاوی بخش کامنت
 # با پیاده سازی
 # functional view
+@login_required
 def book_detail_view(request, pk):
     # get book object
     book = get_object_or_404(Book, pk=pk)
@@ -47,23 +52,21 @@ def book_detail_view(request, pk):
         'comment_form':comment_form
     ,})
 
-
-
 #     نمایش صفحه حاوی فرم ساخت کتاب به کاربر
-class BookCreateView(generic.CreateView):
+class BookCreateView( LoginRequiredMixin, generic.CreateView):
     model = Book
 
     template_name = "books/book_create.html"
     fields = ["title", "author", "descriptions","price","cover"]
 
 #     نمایش صفخه برای اینکه کاربر بتواند مااب های خود را ویرایش کند
-class BookUpdateView(generic.UpdateView):
+class BookUpdateView( LoginRequiredMixin, generic.UpdateView):
     model = Book
     fields = ["title", "author", "descriptions","cover"]
     template_name = 'books/book_update.html'
 
 # نمایش صفحه ای برای حذف متاب توسط کاربر
-class BookDeleteView(generic.DeleteView):
+class BookDeleteView( LoginRequiredMixin, generic.DeleteView):
     model = Book
     template_name = "books/book_delete.html"
     # بعد ااز حذف کتاب مد نطر کاربر ان را به صفحه لیست کتاب ها ببر
